a37267d5511c505e136852484319d69209b298d1
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-09-11 06:29:40 -0300

    Criando classe de instancias estruturais de dados e subcunjuntos de acesso da sonda

---
+++ src/main/java/com/api/project/mapperMarsPhotoRover/model/MarsPhoto.java
@@ -0,0 +1,75 @@
+package com.api.project.mapperMarsPhotoRover.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class MarsPhoto {
+
+	private Long id;
+
+	private Integer sol;
+
+	private Cameras camera;
+
+	@JsonProperty("img_src")
+	private String img;
+
+	@JsonProperty("earth_date")
+	private String earth_date;
+
+	private MarsRover rover;
+	
+	public MarsPhoto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getSol() {
+		return sol;
+	}
+
+	public void setSol(Integer sol) {
+		this.sol = sol;
+	}
+
+	public Cameras getCamera() {
+		return camera;
+	}
+
+	public void setCamera(Cameras camera) {
+		this.camera = camera;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getEarth_date() {
+		return earth_date;
+	}
+
+	public void setEarth_date(String earth_date) {
+		this.earth_date = earth_date;
+	}
+
+	public MarsRover getRover() {
+		return rover;
+	}
+
+	public void setRover(MarsRover rover) {
+		this.rover = rover;
+	}
+	
+	
+
+}
