6ec71ed135eaf79327f44993e3fba1ac2825fd28
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-09-11 05:35:17 -0300

    Iniciando projeto de mapeamento de dados do projeto mars roves

---
+++ src/main/java/com/api/project/mapperMarsPhotoRover/MapperMarsPhotoRoverApplication.java
@@ -0,0 +1,13 @@
+package com.api.project.mapperMarsPhotoRover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapperMarsPhotoRoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapperMarsPhotoRoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/project/mapperMarsPhotoRover/MapperMarsPhotoRoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.project.mapperMarsPhotoRover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapperMarsPhotoRoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
