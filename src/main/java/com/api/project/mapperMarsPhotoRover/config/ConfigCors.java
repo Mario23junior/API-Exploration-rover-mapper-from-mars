bedc07719341b162922bcdaebf50608c611fbdd6
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-09-15 12:24:43 -0300

    Reconfigurando acesso de rota do CORs

--- src/main/java/com/api/project/mapperMarsPhotoRover/config/ConfigCors.java
+++ src/main/java/com/api/project/mapperMarsPhotoRover/config/ConfigCors.java
@@ -1,15 +1,22 @@
 package com.api.project.mapperMarsPhotoRover.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class ConfigCors implements WebMvcConfigurer {
-   
-	 public  void AddCorsMappins(CorsRegistry registry) {
-		 registry.addMapping("/**");
-	 }
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("*")
+                .maxAge(3600L)
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
 }
